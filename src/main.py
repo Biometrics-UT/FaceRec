df9d93fa975a0482fb0eb5fd9b92f0e0f85ca98d
Erwan Kessler <45232488+erwan-kessler@users.noreply.github.com>
2020-12-12 11:27:34 +0100

    Some more detection

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import cv2
 from pathlib import *
 import numpy as np
+import face_recognition
 
 directory_path = Path("../data/fgrc_dataset/Spring2003")
 cascade_path = Path("../models/default_frontalface.xml")
@@ -31,9 +32,10 @@ list_files = [f for f in directory_path.glob('**/*.jpg') if f.is_file()]
 
 file = np.random.choice(list_files)
 print(f"Using {file} as the file")
+## OPENCV
+file_path_str = file.absolute().__str__()
+image = cv2.imread(file_path_str)
 
-image = cv2.imread(file.absolute().__str__())
-image = resize_image(image, height=600)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 faces = faceCascade.detectMultiScale(
     gray,
@@ -42,11 +44,78 @@ faces = faceCascade.detectMultiScale(
     minSize=(30, 30)
 )
 
-print("Found {0} faces!".format(len(faces)))
+print(f"Found {len(faces)} faces!")
+print(*faces)
 
-# Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-cv2.imshow("Faces found", image)
 
-cv2.waitKey(0)
+def draw_faces(_image, _faces):
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in _faces:
+        cv2.rectangle(_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    _image = resize_image(_image, height=600)
+    cv2.imshow("Faces found", _image)
+    cv2.waitKey(0)
+
+
+# draw_faces(image,faces)
+
+## FACE RECOGNITION
+def draw_faces_scaled(face_locations, face_names, face_landmarks_list, frame, scale=4):
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    for (top, right, bottom, left), name, face_landmarks in zip(face_locations, face_names, face_landmarks_list):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= scale
+        right *= scale
+        bottom *= scale
+        left *= scale
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        # Draw some eyebrow
+        cv2.polylines(frame, np.int32([face_landmarks['left_eyebrow']])*scale, isClosed=True, color=(68, 54, 39, 128))
+        cv2.polylines(frame, np.int32([face_landmarks['right_eyebrow']])*scale, isClosed=True, color=(68, 54, 39, 128))
+        cv2.polylines(frame, np.int32([face_landmarks['left_eyebrow']])*scale, isClosed=True, color=(68, 54, 39, 150), thickness=5)
+        cv2.polylines(frame, np.int32([face_landmarks['right_eyebrow']])*scale, isClosed=True, color=(68, 54, 39, 150), thickness=5)
+
+        # Gloss the lips
+        cv2.polylines(frame, np.int32([face_landmarks['top_lip']])*scale, isClosed=True, color=(150, 0, 0, 128))
+        cv2.polylines(frame, np.int32([face_landmarks['bottom_lip']])*scale, isClosed=True, color=(150, 0, 0, 128))
+        cv2.polylines(frame, np.int32([face_landmarks['top_lip']])*scale, isClosed=True, color=(150, 0, 0, 64), thickness=5)
+        cv2.polylines(frame, np.int32([face_landmarks['bottom_lip']])*scale, isClosed=True, color=(150, 0, 0, 64), thickness=5)
+
+        # Sparkle the eyes
+        cv2.polylines(frame, np.int32([face_landmarks['left_eye']])*scale, isClosed=True, color=(255, 255, 255, 30))
+        cv2.polylines(frame, np.int32([face_landmarks['right_eye']])*scale, isClosed=True, color=(255, 255, 255, 30))
+
+    _image = resize_image(frame, height=600)
+    cv2.imshow("Faces found", _image)
+    cv2.waitKey(0)
+
+
+image = face_recognition.load_image_file(file_path_str)
+image_small = cv2.resize(image, (0, 0), fx=0.25, fy=0.25)
+image_small = image_small[:, :, ::-1]  # BGR to RGB (opencv fix)
+known_face_encodings = []
+known_face_names = []
+face_locations = face_recognition.face_locations(image_small)
+face_landmarks_list = face_recognition.face_landmarks(image_small)
+face_encodings = face_recognition.face_encodings(image_small, face_locations)
+face_names = []
+for face_encoding in face_encodings:
+    # See if the face is a match for the known face(s)
+    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+    name = "Unknown"
+    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+    if len(face_distances):
+        best_match_index: int = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+
+    face_names.append(name)
+
+print(face_locations)
+print(face_landmarks_list)
+
+draw_faces_scaled(face_locations, face_names, face_landmarks_list, image)
