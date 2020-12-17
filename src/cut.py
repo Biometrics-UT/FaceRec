c349df78b37381cbd7e59a3c0f4c0e167347b5aa
Erwan KESSLER <45232488+erwan-kessler@users.noreply.github.com>
2020-12-17 14:27:27 +0100

    cut example

---
+++ src/cut.py
@@ -0,0 +1,39 @@
+import time
+
+import cv2
+from pathlib import *
+import numpy as np
+import face_recognition
+from PIL import Image
+
+directory_path = Path("../data/nd1/Spring2003")
+out_path = Path("../data/out")
+out_path.mkdir(exist_ok=True)
+
+assert directory_path.exists() and directory_path.is_dir()
+assert out_path.exists() and out_path.is_dir()
+
+list_files = [f for f in directory_path.glob('**/*.jpg') if f.is_file()]
+# file: Path = np.random.choice(list_files)
+t = time.time()
+nbr = len(list_files)
+for i, file in enumerate(list_files):
+    # print(f"Using {file} as the file")
+    file_path_str = file.absolute().__str__()
+    image = face_recognition.load_image_file(file_path_str)
+    face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model="hog")  # cnn if gpu
+    # print("I found {} face(s) in this photograph.".format(len(face_locations)))
+
+    for j, face_location in enumerate(face_locations):
+        # Print the location of each face in this image
+        top, right, bottom, left = face_location
+        # print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
+
+        # You can access the actual face itself like this:
+        face_image = image[top:bottom, left:right]
+        pil_image = Image.fromarray(face_image)
+        # print(out_path / f"{file.stem}_{i}{file.suffix}")
+        pil_image.save(out_path / f"{file.stem}_{j}{file.suffix}")
+    if i % (nbr // 100) == 0:
+        print(f"Done at {i / nbr * 100:.2f}% in {time.time() - t:.2f}s")
+print(f"Done at 100% in {time.time() - t:.2f}s")
--- src/main.py
+++ src/main.py
@@ -3,10 +3,7 @@ from pathlib import *
 import numpy as np
 import face_recognition
 
-directory_path = Path("../data/fgrc_dataset/Spring2003")
-cascade_path = Path("../models/default_frontalface.xml")
-
-assert cascade_path.exists() and cascade_path.is_file()
+directory_path = Path("../data/nd1/Spring2003")
 assert directory_path.exists() and directory_path.is_dir()
 
 
@@ -25,8 +22,7 @@ def resize_image(_image, width=None, height=None, inter=cv2.INTER_AREA):
     return cv2.resize(_image, dim, interpolation=inter)
 
 
-print(cascade_path.absolute().__str__())
-faceCascade = cv2.CascadeClassifier(cascade_path.absolute().__str__())
+
 
 list_files = [f for f in directory_path.glob('**/*.jpg') if f.is_file()]
 
@@ -34,18 +30,25 @@ file = np.random.choice(list_files)
 print(f"Using {file} as the file")
 ## OPENCV
 file_path_str = file.absolute().__str__()
-image = cv2.imread(file_path_str)
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30)
-)
 
-print(f"Found {len(faces)} faces!")
-print(*faces)
+def opencv(file_path_str):
+    cascade_path = Path("../models/default_frontalface.xml")
+    assert cascade_path.exists() and cascade_path.is_file()
+    faceCascade = cv2.CascadeClassifier(cascade_path.absolute().__str__())
+    image = cv2.imread(file_path_str)
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+
+    print(f"Found {len(faces)} faces!")
+    print(*faces)
+    return image,faces
 
 
 def draw_faces(_image, _faces):
@@ -74,20 +77,25 @@ def draw_faces_scaled(face_locations, face_names, face_landmarks_list, frame, sc
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
         # Draw some eyebrow
-        cv2.polylines(frame, np.int32([face_landmarks['left_eyebrow']])*scale, isClosed=True, color=(68, 54, 39, 128))
-        cv2.polylines(frame, np.int32([face_landmarks['right_eyebrow']])*scale, isClosed=True, color=(68, 54, 39, 128))
-        cv2.polylines(frame, np.int32([face_landmarks['left_eyebrow']])*scale, isClosed=True, color=(68, 54, 39, 150), thickness=5)
-        cv2.polylines(frame, np.int32([face_landmarks['right_eyebrow']])*scale, isClosed=True, color=(68, 54, 39, 150), thickness=5)
+        cv2.polylines(frame, np.int32([face_landmarks['left_eyebrow']]) * scale, isClosed=True, color=(68, 54, 39, 128))
+        cv2.polylines(frame, np.int32([face_landmarks['right_eyebrow']]) * scale, isClosed=True,
+                      color=(68, 54, 39, 128))
+        cv2.polylines(frame, np.int32([face_landmarks['left_eyebrow']]) * scale, isClosed=True, color=(68, 54, 39, 150),
+                      thickness=5)
+        cv2.polylines(frame, np.int32([face_landmarks['right_eyebrow']]) * scale, isClosed=True,
+                      color=(68, 54, 39, 150), thickness=5)
 
         # Gloss the lips
-        cv2.polylines(frame, np.int32([face_landmarks['top_lip']])*scale, isClosed=True, color=(150, 0, 0, 128))
-        cv2.polylines(frame, np.int32([face_landmarks['bottom_lip']])*scale, isClosed=True, color=(150, 0, 0, 128))
-        cv2.polylines(frame, np.int32([face_landmarks['top_lip']])*scale, isClosed=True, color=(150, 0, 0, 64), thickness=5)
-        cv2.polylines(frame, np.int32([face_landmarks['bottom_lip']])*scale, isClosed=True, color=(150, 0, 0, 64), thickness=5)
+        cv2.polylines(frame, np.int32([face_landmarks['top_lip']]) * scale, isClosed=True, color=(150, 0, 0, 128))
+        cv2.polylines(frame, np.int32([face_landmarks['bottom_lip']]) * scale, isClosed=True, color=(150, 0, 0, 128))
+        cv2.polylines(frame, np.int32([face_landmarks['top_lip']]) * scale, isClosed=True, color=(150, 0, 0, 64),
+                      thickness=5)
+        cv2.polylines(frame, np.int32([face_landmarks['bottom_lip']]) * scale, isClosed=True, color=(150, 0, 0, 64),
+                      thickness=5)
 
         # Sparkle the eyes
-        cv2.polylines(frame, np.int32([face_landmarks['left_eye']])*scale, isClosed=True, color=(255, 255, 255, 30))
-        cv2.polylines(frame, np.int32([face_landmarks['right_eye']])*scale, isClosed=True, color=(255, 255, 255, 30))
+        cv2.polylines(frame, np.int32([face_landmarks['left_eye']]) * scale, isClosed=True, color=(255, 255, 255, 30))
+        cv2.polylines(frame, np.int32([face_landmarks['right_eye']]) * scale, isClosed=True, color=(255, 255, 255, 30))
 
     _image = resize_image(frame, height=600)
     cv2.imshow("Faces found", _image)
